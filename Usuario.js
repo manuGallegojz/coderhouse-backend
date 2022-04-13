26b90e40143f8ed292d600e2d50255bdd3dec77c
Manuel Gallego <man.gallegojz@outlook.com>
2022-04-12 23:27:42 -0300

    Desafio3

--- Usuario.js
+++ Usuario.js
@@ -1,156 +1,81 @@
-const { count } = require("console");
+const express = require("express");
+const http = require("http");
+const path = require("path");
 const fs = require("fs");
+const PORT = process.env.PORT || 8080
+const app = express()
 
-let productos = [                                                                                                                                                     
-    {                                                                                                                                                    
-        title: 'Escuadra',                                                                                                                                 
-        price: 123.45,                                                                                                                                     
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',                                     
-        id: 1                                                                                                                                              
-    },                                                                                                                                                   
-    {                                                                                                                                                    
-        title: 'Calculadora',                                                                                                                              
-        price: 234.56,                                                                                                                                     
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',                                          
-        id: 2                                                                                                                                              
-    },                                                                                                                                                   
-    {                                                                                                                                                    
-        title: 'Globo Terráqueo',                                                                                                                          
-        price: 345.67,                                                                                                                                     
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',                                   
-        id: 3                                                                                                                                              
-    }                                                                                                                                                    
+//localhost 8080
+
+app.listen(PORT)
+
+//productos
+
+let productos = [
+    {
+        title: 'Escuadra',
+        price: 123.45,
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+        id: 1
+    },
+    {
+        title: 'Calculadora',
+        price: 234.56,
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
+        id: 2
+    },
+    {
+        title: 'Globo Terráqueo',
+        price: 345.67,
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
+        id: 3
+    }
 ]
 
-fs.readFile("./productos.json", "utf-8", (err, data)=>{
-    if(err){
-        console.error("Error al escribir.")
-    }
-    if(data){
-        fs.writeFile("./productos.json", JSON.stringify(productos), {encoding: "utf-8"}, (err)=>{
-            if(err){
-                console.error("Error al escribir.   ")
-            }
-        })
-    }
-})
+// fs.readFile("./productos.json", "utf-8", (err, data)=>{
+//     if(err){
+//         console.error("Error al escribir.")
+//     }
+//     if(data){
+//         fs.writeFile("./productos.json", JSON.stringify(productos), {encoding: "utf-8"}, (err)=>{
+//             if(err){
+//                 console.error("Error al escribir.   ")
+//             }
+//         })
+//     }
+// })
 
-class Contenedor{
-    constructor(archivo){
-        this.archivo = archivo;
-    }
+//desafio 3
 
-    save(objeto) //: Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-    {
-        fs.readFile(`./${this.archivo}`, "utf-8", (err, data)=>{
-            if(err){
-                console.error("Error al leer.");
-            }else
-            {
+app.get("/", (req, res)=>{
 
-                let productos = JSON.parse(data);
+    res.sendFile(path.join(__dirname + "/index.html"));
 
-                if(productos.length !== 4){
+    })
 
-                    productos.push(objeto);
+app.get("/productos", (req, res)=>{
 
-                    console.log(productos);
-
-                    fs.writeFile(`./${this.archivo}`, JSON.stringify(productos), "utf-8", (error) =>{
-                        if(error){
-                            console.log("Se produjo un error")
-                            }
-                    }
-                    )
-
-            }
+    fs.readFile(`./productos.json`, "utf-8", (err, data)=>{
+        if(err){
+            console.error("Error al leer.")
+        }else
+        {
+                const datos = JSON.parse(data);
+                res.send(datos);
         }
-        })
+    })
+    })
 
+app.get("/productoRandom", (req, res)=>{
 
-    }
-
-    getById(Number) //: Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
-    {
-        fs.readFile(`./${this.archivo}`, "utf-8", (err, data)=>{
-            if(err){
-                console.error("Error al leer.")
-            }else
-            {
-                let encontrarProducto = JSON.parse(data).find(x => {
-                    return x.id == Number
-                })
-                console.log(encontrarProducto);
-            }
-        })
+    fs.readFile(`./productos.json`, "utf-8", (err, data)=>{
+        if(err){
+            console.error("Error al leer.")
+        }else
+        {
+            const datos = JSON.parse(data);
+            const datoRandom = datos[Math.floor(Math.random() * datos.length)];
+            res.send(datoRandom);
         }
-
-    getAll() //: Object[] - Devuelve un array con los objetos presentes en el archivo.
-    {
-        fs.readFile(`./${this.archivo}`, "utf-8", (err, data)=>{
-            if(err){
-                console.error("Error al leer.")
-            }else
-            {
-                console.log(JSON.parse(data))
-            }
-        })
-        }
-
-    deleteById(Number) //: void - Elimina del archivo el objeto con el id buscado.
-    {
-        fs.readFile(`./${this.archivo}`, "utf-8", (err, data)=>{
-            if(err){
-                console.error("Error al leer.")
-            }else
-            {
-                let ListaSinProducto = JSON.parse(data).filter(x => {
-                    return x.id != Number
-                })
-                fs.writeFile(`./${this.archivo}`, JSON.stringify(ListaSinProducto), "utf-8", (error) =>{
-                    if(error){
-                        console.log("Se produjo un error")
-                        }
-                        console.log(ListaSinProducto)
-                })
-            }
-        })
-        }
-
-    deleteAll() //: void - Elimina todos los objetos presentes en el archivo.
-    {
-        fs.readFile(`./${this.archivo}`, "utf-8", (err, data)=>{
-            if(err){
-                console.error("Error al leer.")
-            }else
-            {
-                let ListaLimpia = [];
-                fs.write(`./${this.archivo}`, JSON.stringify(ListaLimpia), "utf-8", (error) =>{
-                    if(error){
-                        console.log("Se produjo un error")
-                        }
-                        console.log(ListaLimpia)
-                })
-                
-            }
-        })
-        }
-    
-    }
-
-            const archivo = new Contenedor("productos.json");
-
-            archivo.save({                                                                                                                                  
-                title: 'NotePad',                                                                                                                              
-                price: 129.59,                                                                                                                                     
-                thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',                                          
-                id: 4
-                })
-
-            archivo.getById(2)
-
-            archivo.getAll()
-
-            //archivo.deleteById(2)
-
-            //archivo.deleteAll()
+    })
+    })
